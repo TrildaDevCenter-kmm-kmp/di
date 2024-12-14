ea5847e428d93ec6350e870a589eb91b541901e9
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-14 15:04:14 +0200

    Fix tests

--- di/src/commonTest/kotlin/ivy/di/GenericsWorkaroundTest.kt
+++ di/src/commonTest/kotlin/ivy/di/GenericsWorkaroundTest.kt
@@ -1,8 +1,7 @@
 package ivy.di
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.shouldBeInstanceOf
+import io.kotest.matchers.shouldNotBe
 import ivy.di.Di.register
 import kotlin.jvm.JvmInline
 import kotlin.test.BeforeTest
@@ -10,53 +9,55 @@ import kotlin.test.Test
 
 class GenericsWorkaroundTest {
 
-    @BeforeTest
-    fun setup() {
-        Di.reset()
+  @BeforeTest
+  fun setup() {
+    Di.reset()
+  }
+
+  @Test
+  fun pre_workaround_behavior() {
+    // Given
+    Di.appScope {
+      register { Container(42) }
+      register { Container("hello") }
     }
 
-    @Test
-    fun pre_workaround_behavior() {
-        // Given
-        Di.appScope {
-            register { Container(42) }
-            register { Container("hello") }
-        }
+    // When
+    val intContainer = Di.get<Container<Int>>()
+    val stringContainer = Di.get<Container<String>>()
 
-        // When
-        val intContainer = Di.get<Container<Int>>()
-        val stringContainer = Di.get<Container<String>>()
+    // Then
+    // Not valid on all platforms (e.g. Linux)
+//        intContainer.value.shouldBeInstanceOf<String>() // very weird, indeed!
+//        shouldThrow<ClassCastException> {
+//            intContainer.value + 1
+//        }
+    intContainer.value shouldNotBe 42
+    stringContainer.value shouldBe "hello"
+  }
 
-        // Then
-        intContainer.value.shouldBeInstanceOf<String>() // very weird, indeed!
-        shouldThrow<ClassCastException> {
-            intContainer.value + 1
-        }
-        stringContainer.value shouldBe "hello"
+  @Test
+  fun workaround_is_working() {
+    // Given
+    Di.appScope {
+      register { IntContainer(Container(42)) }
+      register { StringContainer(Container("hello")) }
     }
 
-    @Test
-    fun workaround_is_working() {
-        // Given
-        Di.appScope {
-            register { IntContainer(Container(42)) }
-            register { StringContainer(Container("hello")) }
-        }
+    // When
+    val intContainer = Di.get<IntContainer>().value
+    val stringContainer = Di.get<StringContainer>().value
 
-        // When
-        val intContainer = Di.get<IntContainer>().value
-        val stringContainer = Di.get<StringContainer>().value
+    // Then
+    intContainer.value shouldBe 42
+    stringContainer.value shouldBe "hello"
+  }
 
-        // Then
-        intContainer.value shouldBe 42
-        stringContainer.value shouldBe "hello"
-    }
+  class Container<T>(val value: T)
 
-    class Container<T>(val value: T)
+  @JvmInline
+  value class IntContainer(val value: Container<Int>)
 
-    @JvmInline
-    value class IntContainer(val value: Container<Int>)
-
-    @JvmInline
-    value class StringContainer(val value: Container<String>)
+  @JvmInline
+  value class StringContainer(val value: Container<String>)
 }
